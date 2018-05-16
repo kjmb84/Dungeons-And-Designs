5a043584a8e7a0791d441d9e867f2b6eca41c8a6
Kean Mattingly <kjmb84@mail.missouri.edu>
2018-05-15 22:18:51 -0500

    cors was working for a brief moment in space

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  apiUrl: 'localhost:3000/api',
+  apiUrl: 'http://localhost:3000/api',
   production: false
 };
