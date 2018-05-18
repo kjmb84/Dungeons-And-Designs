b981e039c2267edbe7237cb8a3883de4c0ffbd77
Kean Mattingly <kjmb84@mail.missouri.edu>
2018-05-18 16:30:17 -0500

    recurse recurse!

--- src/app/pipes/ability-true-score.pipe.ts
+++ src/app/pipes/ability-true-score.pipe.ts
@@ -8,30 +8,54 @@ import IAbility from '../models/character/IAbility';
 export class AbilityTrueScorePipe implements PipeTransform {
   abilityTrueScores = [];
 
+
   transform(abilities, character: ICharacter): any {
     for (const ability of abilities) {
-      const upgradedAbility: IAbility & { breakdown: object } = ability;
-      upgradedAbility.breakdown = this.recursiveAttributeFindFromCharacterInterface(upgradedAbility.name, character, upgradedAbility, {});
-      this.abilityTrueScores.push(upgradedAbility);
+      // console.log(character);
+      this.recursiveAttributeFindFromCharacterInterface(character);
+      // const upgradedAbility: IAbility & { breakdown: object } = ability;
+    // upgradedAbility.breakdown = this.recursiveAttributeFindFromCharacterInterface(upgradedAbility.name, character, upgradedAbility, {});
+      // this.abilityTrueScores.push(upgradedAbility);
       // console.log(upgradedAbility);
       // this.abilityTrueScores.push(this.recursiveAttributeFindFromCharacterInterface(ability.name, character, ability));
     }
     console.log(this.abilityTrueScores);
 
-    return this.abilityTrueScores;
+    return abilities;
+    // return this.abilityTrueScores;
   }
 
-  recursiveAttributeFindFromCharacterInterface(abilityName, obj: Object, ability, abilityTrueScore) {
-    if (!abilityTrueScore.score) {
-      abilityTrueScore = {
-        score: ability.score,
-        breakdown: [],
-      };
-    }
+  recursiveAttributeFindFromCharacterInterface(obj) {
+   
+    for (const property in obj) {
+      if (obj.hasOwnProperty(property)) {
+          if (property === 'attributes' && Array.isArray(obj[property])) {
+            console.log(obj[property]);
+            return;
+          } else if (typeof obj[property] === 'object') {
+            this.recursiveAttributeFindFromCharacterInterface(obj[property]);
+          } else {
+              // console.log(property + "   " + obj[property]);
+          }
+      }
+  }
+   
+   
+   
+    // if (!abilityTrueScore.score) {
+    //   abilityTrueScore = {
+    //     score: ability.score,
+    //     breakdown: [],
+    //   };
+    // }
 
-    const keys = Object.keys(obj);
+    // const keys = Object.keys(obj);
 
-    console.log(keys);
+    // if (keys[0] === 'attributes') {
+    //   console.log(obj[keys[0]]);
+    // } else {
+    //   this.recursiveAttributeFindFromCharacterInterface(abilityName, )
+    // }
       // console.log(Object.keys(obj));
         // this.recursiveAttributeFindFromCharacterInterface(abilityName, obj, ability, abilityTrueScore);
 
@@ -58,34 +82,34 @@ export class AbilityTrueScorePipe implements PipeTransform {
 
 
 
-      const x = true;
-      if (x) { return 0; }
+      // const x = true;
+      // if (x) { return 0; }
 
 
     // for (const key of Object.keys(obj)) {
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        const newObj: any = (<any>obj)[key];
-        console.log(key);
-        if (key === 'attributes') {
-            if (Array.isArray(newObj)) {
-              for (const attribute of newObj) {
+    // for (const key in obj) {
+    //   if (obj.hasOwnProperty(key)) {
+    //     const newObj: any = (<any>obj)[key];
+    //     console.log(key);
+    //     if (key === 'attributes') {
+    //         if (Array.isArray(newObj)) {
+    //           for (const attribute of newObj) {
 
-                if (attribute.name === abilityName) {
-                  abilityTrueScore.score += attribute.score;
-                  abilityTrueScore.breakdown.push(attribute);
-                }
-              }
-            } else if (typeof newObj === 'object') {
-                console.log(newObj);
-            }
-        } else if (typeof newObj === 'object') {
-            this.recursiveAttributeFindFromCharacterInterface(newObj, abilityName, ability, abilityTrueScore);
-        }
-      }
-    }
+    //             if (attribute.name === abilityName) {
+    //               abilityTrueScore.score += attribute.score;
+    //               abilityTrueScore.breakdown.push(attribute);
+    //             }
+    //           }
+    //         } else if (typeof newObj === 'object') {
+    //             console.log(newObj);
+    //         }
+    //     } else if (typeof newObj === 'object') {
+    //         this.recursiveAttributeFindFromCharacterInterface(newObj, abilityName, ability, abilityTrueScore);
+    //     }
+    //   }
+    // }
 
-    return abilityTrueScore;
+    // return abilityTrueScore;
 }
 
 }
