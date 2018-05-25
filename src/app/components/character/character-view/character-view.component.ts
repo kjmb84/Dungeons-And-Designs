77998bd5cd5a1db19f8401bce0519314616823f5
Kean Mattingly <kjmb84@mail.missouri.edu>
2018-05-24 23:11:56 -0500

    ability true score pipe more or less working correctly

--- src/app/components/character/character-view/character-view.component.ts
+++ src/app/components/character/character-view/character-view.component.ts
@@ -14,7 +14,7 @@ export class CharacterViewComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-
+    
   }
 
 }
--- src/app/pipes/ability-true-score.pipe.ts
+++ src/app/pipes/ability-true-score.pipe.ts
@@ -1,6 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import ICharacter from '../models/character/ICharacter';
 import IAbility from '../models/character/IAbility';
+import { isObject, isArray } from 'util';
 
 @Pipe({
   name: 'abilityTrueScore'
@@ -8,108 +9,50 @@ import IAbility from '../models/character/IAbility';
 export class AbilityTrueScorePipe implements PipeTransform {
   abilityTrueScores = [];
 
-
-  transform(abilities, character: ICharacter): any {
+  transform(abilities: UpgradedAbility[], character: ICharacter): any {
+    console.log(character);
+    
     for (const ability of abilities) {
-      // console.log(character);
-      this.recursiveAttributeFindFromCharacterInterface(character);
-      // const upgradedAbility: IAbility & { breakdown: object } = ability;
-    // upgradedAbility.breakdown = this.recursiveAttributeFindFromCharacterInterface(upgradedAbility.name, character, upgradedAbility, {});
-      // this.abilityTrueScores.push(upgradedAbility);
-      // console.log(upgradedAbility);
-      // this.abilityTrueScores.push(this.recursiveAttributeFindFromCharacterInterface(ability.name, character, ability));
+      ability.breakdown = [];
+      this.abilityTrueScores.push(this.recursiveAttributeFindFromCharacterInterface(ability, character));
     }
+
     console.log(this.abilityTrueScores);
 
     return abilities;
-    // return this.abilityTrueScores;
   }
 
-  recursiveAttributeFindFromCharacterInterface(obj) {
-   
-    for (const property in obj) {
-      if (obj.hasOwnProperty(property)) {
-          if (property === 'attributes' && Array.isArray(obj[property])) {
-            console.log(obj[property]);
-            return;
-          } else if (typeof obj[property] === 'object') {
-            this.recursiveAttributeFindFromCharacterInterface(obj[property]);
-          } else {
-              // console.log(property + "   " + obj[property]);
-          }
+  recursiveAttributeFindFromCharacterInterface(ability: UpgradedAbility, obj): UpgradedAbility {
+    if (isObject(obj)) {
+      for (const key of Object.keys(obj)) {
+        const newObj = obj[key];
+        if (key === 'attributes') {
+          ability = this.addModifiersToAbility(ability, newObj, obj);
+        } else if (isArray(obj[key])) {
+          newObj.forEach(index => ability = this.recursiveAttributeFindFromCharacterInterface(ability, index));
+        } else if (isObject(obj[key])) {
+          ability = this.recursiveAttributeFindFromCharacterInterface(ability, obj[key]);
+        }
       }
+    }
+
+    return ability;
   }
-   
-   
-   
-    // if (!abilityTrueScore.score) {
-    //   abilityTrueScore = {
-    //     score: ability.score,
-    //     breakdown: [],
-    //   };
-    // }
 
-    // const keys = Object.keys(obj);
+  addModifiersToAbility(ability, attribute, parent) {
+    if (isArray(attribute)) {
+      attribute.forEach(modifier => {
+        if (ability.name === modifier.name) {
+          ability.score += modifier.modifier;
+          ability.breakdown.push(attribute);
+        }
+      });
+    }
+    return ability;
+  }
 
-    // if (keys[0] === 'attributes') {
-    //   console.log(obj[keys[0]]);
-    // } else {
-    //   this.recursiveAttributeFindFromCharacterInterface(abilityName, )
-    // }
-      // console.log(Object.keys(obj));
-        // this.recursiveAttributeFindFromCharacterInterface(abilityName, obj, ability, abilityTrueScore);
-
-      // for (const key of Object.keys(obj)) {
-      //   const subObject: any = (<any>obj)[key];
-        
-        // if (key === 'attributes') {
-          // console.log('attribute');
-          // if (Array.isArray(subObject)) {
-          //   for (const attribute of subObject) {
-          //     if (attribute.name === abilityName) {
-          //       // console.log('it\'s a match', abilityName);
-          //     }
-          //   }
-          // } else if (typeof subObject === 'object') {
-          //   // console.log('subobject', subObject);
-          // }
-        // } else if (typeof subObject === 'object') {
-          // console.log('non-attribute', subObject);
-          
-          // this.recursiveAttributeFindFromCharacterInterface(subObject, abilityName, ability, abilityTrueScore);
-        // }
-      // }
-
-
-
-      // const x = true;
-      // if (x) { return 0; }
-
-
-    // for (const key of Object.keys(obj)) {
-    // for (const key in obj) {
-    //   if (obj.hasOwnProperty(key)) {
-    //     const newObj: any = (<any>obj)[key];
-    //     console.log(key);
-    //     if (key === 'attributes') {
-    //         if (Array.isArray(newObj)) {
-    //           for (const attribute of newObj) {
-
-    //             if (attribute.name === abilityName) {
-    //               abilityTrueScore.score += attribute.score;
-    //               abilityTrueScore.breakdown.push(attribute);
-    //             }
-    //           }
-    //         } else if (typeof newObj === 'object') {
-    //             console.log(newObj);
-    //         }
-    //     } else if (typeof newObj === 'object') {
-    //         this.recursiveAttributeFindFromCharacterInterface(newObj, abilityName, ability, abilityTrueScore);
-    //     }
-    //   }
-    // }
-
-    // return abilityTrueScore;
 }
 
+interface UpgradedAbility extends IAbility {
+  breakdown: Array<object>;
 }
