ed30db51695b3cbcf7c64adb20e26bdd391a11e3
Kean Mattingly <kean.mattingly@gmail.com>
2018-07-11 07:45:18 -0500

    work on move validation in map object

--- src/app/classes/map-object.ts
+++ src/app/classes/map-object.ts
@@ -1,14 +1,22 @@
+import { MapService } from './../services/map/map.service';
+import { MapCanvas } from './map-canvas';
 import { MapSquareDirection } from './../enums/map-square-directions';
 import { MapCoordinates } from './map-coordinates';
 
 export class MapObject {
   coordinates: MapCoordinates;
   history: MapCoordinates[];
+  _mapService: MapService;
+  mapCanvas: MapCanvas;
 
-  move(direction: MapSquareDirection): MapCoordinates {
+  constructor(mapService: MapService) {
+    this._mapService = mapService;
+    this.mapCanvas = this._mapService.get();
+  }
+
+  move(direction: MapSquareDirection): void {
     const previousCell: MapCoordinates = Object.assign({}, this.coordinates);
     if (direction !== undefined) {
-      this.history.push(previousCell);
       switch (direction) {
         case MapSquareDirection.ArrowDown:
           this.coordinates.moveDown();
@@ -23,7 +31,19 @@ export class MapObject {
           this.coordinates.moveRight();
           break;
       }
+      if (this.validMove()) {
+        this.history.push(previousCell);
+      } else {
+        this.coordinates = previousCell;
+      }
     }
-    return this.coordinates;
+  }
+
+  private validMove(): boolean {
+    let valid = false;
+    if (this.coordinates.x > this.mapCanvas.dimensions.x || this.coordinates.y > this.mapCanvas.dimensions.y) {
+      valid = true;
+    }
+    return valid;
   }
 }
